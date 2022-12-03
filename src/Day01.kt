ef07948ead916940dd7bc7492a02270b28cd1f07
mark wu <markwu@oursky.com>
2022-12-03 11:18:50 +0800

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,17 @@
 fun main() {
+    fun getElfCalories(input: List<String>): List<Int> {
+        return input.joinToString(separator = "\n").split("\n\n").map { it ->
+            it.split("\n").sumOf { it.toInt() }
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return getElfCalories(input).maxOf { it }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return getElfCalories(input).sortedDescending().take(3).sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
